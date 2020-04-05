0351780b88345c6932d5c8270415a44f653779fa
WORD559 <josh.barrass.work@gmail.com>
2020-04-05 15:06:42 +0100

    Player ID and Name

--- src/pkg/common/player-local.go
+++ src/pkg/common/player-local.go
@@ -4,6 +4,8 @@ package common
 // server, all players will be of this type. In a client, only the
 // local player should use this type.
 type LocalPlayer struct {
+	ID        int // unique ID assigned by server
+	Name      string
 	Resources []ResourceType
 	// TODO: random cards
 	//Cards []
