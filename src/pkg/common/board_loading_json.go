963b242dfdc249b70f26cee7f01bc3c0654f4c48
WORD559 <josh.barrass.work@gmail.com>
2020-04-09 23:50:04 +0100

    Add JSON tests

--- src/pkg/common/board_loading_json.go
+++ src/pkg/common/board_loading_json.go
@@ -8,6 +8,7 @@ import (
 // JSONMap encodes the information for the JSON-format maps. This is
 // extensible without issue, as pretty much only the tiles field is
 // required
+// TODO: allow array of unit-length strings rather than numbers
 type JSONMap struct {
 	Tiles   [][]byte `json:"tiles"`
 	Numbers [][]int  `json:"numbers,omitempty"`
